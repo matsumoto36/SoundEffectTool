2ad79f0f56887d6c86856d6169696f4111a3ed39
RyoMatsumoto <nkc.ryo.matsumoto@gmail.com>
2019-02-25 00:18:00 +0900

    Rename Projects

--- ClassLibrary/AssemblyInfo.cpp
+++ ClassLibrary/AssemblyInfo.cpp
@@ -6,11 +6,11 @@ using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
 
-[assembly:AssemblyTitleAttribute(L"SoundEffectToolVM")];
+[assembly:AssemblyTitleAttribute(L"ClassLibrary")];
 [assembly:AssemblyDescriptionAttribute(L"")];
 [assembly:AssemblyConfigurationAttribute(L"")];
 [assembly:AssemblyCompanyAttribute(L"")];
-[assembly:AssemblyProductAttribute(L"SoundEffectToolVM")];
+[assembly:AssemblyProductAttribute(L"ClassLibrary")];
 [assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2019")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
